a575658754c36fe0f1871abe4c225f10ed15182e
Esther <11yekinie@gmail.com>
2023-03-28 20:27:32 +0100

    finished bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,21 @@
+bubble = [4,3,78,2,0,2]
+
+def bubble_sort(array)
+    n = array.length
+
+    loop do
+    swapped = false
+
+    for i in 0..n - 2 do 
+        if array[i] > array[i + 1]
+            array[i], array[i+1] = array[i+1], array[i]
+            swapped = true
+        end
+    end
+        break unless swapped
+    end
+    array
+end
+
+p bubble_sort(bubble)
+
